91ce5d3c847cd63e17cf6720eeae9f24fcf481fe
Kateryna <72016853+kapelyna@users.noreply.github.com>
2024-04-12 10:55:48 +0300

    main.adb

--- src/main.adb
+++ src/main.adb
@@ -1,64 +1,94 @@
-procedure main is
+with Ada.Text_IO, GNAT.Semaphores;
+use Ada.Text_IO, GNAT.Semaphores;
+
+with Ada.Containers.Indefinite_Doubly_Linked_Lists;
+use Ada.Containers;
+
+procedure Consumer_Producer is
    package String_Lists is new Indefinite_Doubly_Linked_Lists (String);
    use String_Lists;
 
-   procedure Starter (Manage_Capacity : in Integer; Total_Productss : in Integer;
-                      Total_Producers : in Integer; Total_Consumers : in Integer) is
-      Manage : List;
+   procedure Starter (Storage_Size : in Integer) is
+      Storage : List;
+      Full   : Counting_Semaphore (Storage_Size, Default_Ceiling);
+      Empty  : Counting_Semaphore (0, Default_Ceiling);
+      Access_Storage : Counting_Semaphore (1, Default_Ceiling);
 
-      Access_Manage : Counting_Semaphore (1, Default_Ceiling);
-      Full_Manage   : Counting_Semaphore (Manage_Capacity, Default_Ceiling);
-      Empty_Manage  : Counting_Semaphore (0, Default_Ceiling);
+      task type Consumer is
+         entry Start(Item_Numbers:Integer);
+      end;
 
-      task Producer;
-      task type Producer_Arr is array (1 .. Total_Producers) of Producer;
+      task type Producer is
+         entry Start(Item_Numbers:Integer);
+      end;
 
-      task Consumer;
-      task type Consumer_Arr is array (1 .. Total_Consumers) of Consumer;
-
-      task body Producer is
-      begin
-         for i in 1 .. Total_Productss loop
-            Full_Manage.Seize;
-            Access_Manage.Seize;
-
-            Manage.Append ("produced " & i'Img);
-            Put_Line ("Producer produced " & i'Img);
-
-            Access_Manage.Release;
-            Empty_Manage.Release;
-            delay 1.0;
-         end loop;
-      end Producer;
 
       task body Consumer is
+         Item_Numbers : Integer;
       begin
-         for i in 1 .. Total_Productss loop
-            Empty_Manage.Seize;
-            Access_Manage.Seize;
+           accept Start (Item_Numbers : in Integer) do
+              Consumer.Item_Numbers := Item_Numbers;
+           end Start;
+
+         for i in 1 .. Item_Numbers loop
+            Empty.Seize;
+            Access_Storage.Seize;
 
             declare
-               Product : String := First_Element (Manage);
+               item : String := First_Element (Storage);
             begin
-               Put_Line ("Consumer consumed " & Product);
+               Put_Line ("Took " & item);
             end;
 
-            Manage.Delete_First;
+            Storage.Delete_First;
 
-            Access_Manage.Release;
-            Full_Manage.Release;
+            Access_Storage.Release;
+            Full.Release;
 
-            delay 2.0;
+            delay 0.3;
          end loop;
+
       end Consumer;
 
-      Producers : Producer_Arr;
-      Consumers : Consumer_Arr;
+      task body Producer is
+           Item_Numbers : Integer;
+      begin
+           accept Start (Item_Numbers : in Integer) do
+              Producer.Item_Numbers := Item_Numbers;
+           end Start;
 
-   begin
-      null;
-   end Starter;
+         for i in 1 .. Item_Numbers loop
+            Full.Seize;
+            Access_Storage.Seize;
+
+            Storage.Append ("item " & i'Img);
+            Put_Line ("Added item " & i'Img);
+
+            Access_Storage.Release;
+            Empty.Release;
+            delay 0.1;
+         end loop;
+
+      end Producer;
+      
+      Num_Consumers : constant := 3;
+      Num_Producers : constant := 2;
+
+      Consumers : array (1..Num_Consumers) of Consumer;
+      Producers : array (1..Num_Producers) of Producer;
+      Items_to_add_consumer: array(1..Num_Consumers) of Integer := (4, 5, 6);
+      Items_to_add_producer: array(1..Num_Producers) of Integer := (7,8);
+
+      begin
+         for i in Consumers'Range loop
+            Consumers(i).Start(Items_to_add_consumer(i));
+         end loop;
+      
+         for i in Producers'Range loop
+            Producers(i).Start(Items_to_add_producer(i));
+         end loop;
+      end Starter;
 
 begin
-   Starter (3, 10, 2, 3); 
-end main;
+   Starter (7);
+end Consumer_Producer;
